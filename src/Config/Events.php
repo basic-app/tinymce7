8414d7bd1165e1e19450d80ff6b6f15b32bbdadb
basic-app <dev@basic-app.com>
2026-09-08 23:27:55 +0300

    n/a

--- src/Config/Events.php
+++ src/Config/Events.php
@@ -8,6 +8,6 @@ AdminMenu::on(static function(AdminMenu $event) :void {
     $event->items[lang('Admin.Settings')]['tinymce7-settings'] = [
         'label' => lang('Admin.TinyMCE 7 Settings'),
         'url' => site_url('admin/tinymce7-settings'),
-        'icon' => ['icon' => 'fa fa-regular fa-file-word']
+        'icon' => 'fa fa-regular fa-file-word'
     ];
 });
